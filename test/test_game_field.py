84f66ba8328ec37178a775d84320a3cb3f066478
apolumestny <anpolumestny@gmail.com>
2023-09-06 12:33:50 +0300

    change Player and Game

--- test/test_game_field.py
+++ test/test_game_field.py
@@ -12,10 +12,9 @@ def test_gamefield_is_empty_cell_exists():
     assert empty_game_field.is_empty_cell_exists()
     not_empty_game_field = GameField(dimension=3)
     not_empty_game_field.field = [['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x']]
-    print(not_empty_game_field.field)
     assert not not_empty_game_field.is_empty_cell_exists()
 
 
 def test_gamefield_collect_game_lines():
     game_field = GameField(dimension=3)
-    assert len(game_field.lines) == 8
+    assert len(game_field.collect_game_lines()) == 8
